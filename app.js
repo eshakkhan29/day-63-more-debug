e753ba5e52727391b9f44cc1d0ca0c5fa9e21cac
eshakkhan29 <eshakkhan29@gmail.com>
2022-03-07 09:21:12 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+let str = "";
+// console.log(!!str);
+
+const marks= 60;
+const resubmit = -5; 
+// console.log(!(marks > 0 || resubmit > 0));
+
+const person = {name: "hero", id: 101, address: "BD",postalCode: 40321};
+// console.log(Object.keys(person).length);
+
+const animals = ['cat', 'dog', 'rat'];
+// console.log(animals.includes('Cat'));
+
+array=[2,3,4,5,2];
+const unique = array => array.indexOf('21');
+// console.log(unique);
+
+const add =(para1 , para2) => para2 + para1
+// console.log(add('2',3));
+
+// const b=6 , b=6
+// console.log(b);
+
+const colors = {mango: 'green', grapes: 'black', organe: 'yellow'};
+console.log(colors[grapes])
